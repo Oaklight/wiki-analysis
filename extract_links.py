62222de4d151404bf2075f8918298e7db59ad289
Peng Ding <pding.dp@gmail.com>
2022-03-16 17:16:30 -0700

    v3: add support for sharding

--- extract_links.py
+++ extract_links.py
@@ -4,10 +4,17 @@ import argparse
 import sys
 import json
 
+import re
+from tqdm import tqdm
+from subprocess import check_output
+from glob import glob
+
 
 parser = argparse.ArgumentParser(description='extract backlinks with "main article" hints')
 parser.add_argument('xmlfile', type=str, help='path to decompressed wikidump xml file')
 parser.add_argument('index', type=str, help='path to wikidump index file, for counting purpose')
+parser.add_argument('--folder', default=False, action="store_true",
+                    help='(true | false), if toggled, treat each lines in xmlfiles as xml path')
 parser.add_argument('-o', '--output', type=str,
                     help='path to store output file')
 parser.add_argument('-f', '--frequency', type=str,
@@ -20,16 +27,12 @@ parser.add_argument('-n', '--threads', type=int,
 args = parser.parse_args()
 # get_ipython().system('cat {args.xmlfile} | grep "main article" | wc -l')
 
-import re
-from tqdm import tqdm
-from subprocess import check_output
-
 
 p = re.compile("{{[mM]ain article\|(?P<backlink>[^\<\[\}]*)}}")
 h = re.compile("={2,}(?P<heading>[^\[=]*)={2,}")
 heading_freq = {} # whole headings
 word_freq = {} # words using in headings
-
+main_articles = []
 
 def wc(filename):
     return int(check_output(["wc", "-l", filename]).split()[0])
@@ -39,31 +42,42 @@ pbar = tqdm(total=wc(args.index))
 def map_main_article(dump, path):
     for page in dump:
         for rev in page:
+            links = []
             if rev.text is not None:
+                headings = h.findall(rev.text)
+                words = [w.strip() for hd in headings for w in hd.lower().split()]
+                for each in headings:
+                    each = each.strip()
+                    if each in heading_freq:
+                        heading_freq[each] += 1
+                    else:
+                        heading_freq[each] = 1
+                for each in words:
+                    if each in word_freq:
+                        word_freq[each] += 1
+                    else:
+                        word_freq[each] = 1
+            
                 links = p.findall(rev.text)
                 if len(links) != 0:
                     links = [e.strip() for l in links for e in l.split('|')]
-                    headings = h.findall(rev.text)
-                    words = [w.strip() for hd in headings for w in hd.lower().split()]
-                    for each in headings:
-                        each = each.strip()
-                        if each in heading_freq:
-                            heading_freq[each] += 1
-                        else:
-                            heading_freq[each] = 1
-                    for each in words:
-                        if each in word_freq:
-                            word_freq[each] += 1
-                        else:
-                            word_freq[each] = 1
             
-            pbar.update(1)
             yield page.id, page.title, links
 
+           
+if args.folder:
+    xmlfile = glob(f"{args.xmlfile}/*.xml-*")
+else:
+    xmlfile = [args.xmlfile]
+
+for id, title, links in mwxml.map(map_main_article, xmlfile, threads=args.threads):
+    pbar.update(1)
+    if len(links) != 0:
+        main_articles.append(f"{id} {title} {str(links)}\n")
+
 with open(args.output, 'w') as f:
-    for id, title, links in mwxml.map(map_main_article, [args.xmlfile], threads=args.threads):
-        if len(links) != 0:
-            f.write(f"{id} {title} {str(links)}\n")
+    for each in main_articles:
+        f.write(each)
 
 result = dict(sorted(heading_freq.items(), key=lambda item: item[1], reverse=True))
 with open(args.frequency, 'w') as f:
