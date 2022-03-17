dc698562f7f9f023a18455f72515fecbba9a0747
Peng Ding <pding.dp@gmail.com>
2022-03-16 17:15:09 -0700

    v2 version

--- extract_links.py
+++ extract_links.py
@@ -7,47 +7,69 @@ import json
 
 parser = argparse.ArgumentParser(description='extract backlinks with "main article" hints')
 parser.add_argument('xmlfile', type=str, help='path to decompressed wikidump xml file')
+parser.add_argument('index', type=str, help='path to wikidump index file, for counting purpose')
 parser.add_argument('-o', '--output', type=str,
                     help='path to store output file')
 parser.add_argument('-f', '--frequency', type=str,
                     help='path to store heading frequency')
+parser.add_argument('-w', '--words', type=str,
+                    help='path to store words frequency')
 parser.add_argument('-n', '--threads', type=int,
                     help='number of parallel processes')
 
-
 args = parser.parse_args()
-
 # get_ipython().system('cat {args.xmlfile} | grep "main article" | wc -l')
 
-p = re.compile("={2,}(?P<heading>[^\[=]*)={2,}(?:\n.*){0,2}?{{[mM]ain article\|(?P<backlink>[^\<\[\}]*)}}")
-heading_freq = {}
+import re
+from tqdm import tqdm
+from subprocess import check_output
 
-def get_main_article(p, text):
-    try:
-        results = p.findall(text)
-    except:
-        print(text)
-        results = {}
-    return {t[0].strip(): t[1].split('|') for t in results}
+
+p = re.compile("{{[mM]ain article\|(?P<backlink>[^\<\[\}]*)}}")
+h = re.compile("={2,}(?P<heading>[^\[=]*)={2,}")
+heading_freq = {} # whole headings
+word_freq = {} # words using in headings
+
+
+def wc(filename):
+    return int(check_output(["wc", "-l", filename]).split()[0])
+
+pbar = tqdm(total=wc(args.index))
 
 def map_main_article(dump, path):
     for page in dump:
         for rev in page:
-            main_articles = get_main_article(p, rev.text)
-            for h in main_articles.keys():
-                if h in heading_freq:
-                    heading_freq[h] += 1
-                else:
-                    heading_freq[h] = 1
-            yield page.id, page.title, main_articles
+            if rev.text is not None:
+                links = p.findall(rev.text)
+                if len(links) != 0:
+                    links = [e.strip() for l in links for e in l.split('|')]
+                    headings = h.findall(rev.text)
+                    words = [w.strip() for hd in headings for w in hd.lower().split()]
+                    for each in headings:
+                        each = each.strip()
+                        if each in heading_freq:
+                            heading_freq[each] += 1
+                        else:
+                            heading_freq[each] = 1
+                    for each in words:
+                        if each in word_freq:
+                            word_freq[each] += 1
+                        else:
+                            word_freq[each] = 1
+            
+            pbar.update(1)
+            yield page.id, page.title, links
 
 with open(args.output, 'w') as f:
-    for id, title, article_map in mwxml.map(map_main_article, [args.xmlfile], threads=args.threads):
-        if len(article_map) != 0:
-            f.write(f"{id} {title} {str(article_map)}\n")
+    for id, title, links in mwxml.map(map_main_article, [args.xmlfile], threads=args.threads):
+        if len(links) != 0:
+            f.write(f"{id} {title} {str(links)}\n")
 
 result = dict(sorted(heading_freq.items(), key=lambda item: item[1], reverse=True))
 with open(args.frequency, 'w') as f:
     f.write(json.dumps(result))
 
+result2 = dict(sorted(word_freq.items(), key=lambda item: item[1], reverse=True))
+with open(args.words, 'w') as f:
+    f.write(json.dumps(result2))
 
